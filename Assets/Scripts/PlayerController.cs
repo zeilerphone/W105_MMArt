2250b51432d3bf29b44b5a5746a7bbde1e2b03df
zeilerphone <zeiler@ucsb.edu>
2024-03-03 13:30:56 -0800

    movement fix, working on decorative tiles/sprites

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,19 +7,23 @@ using UnityEngine.UIElements;
 public class PlayerController : MonoBehaviour
 {
     public InputAction MoveAction;
+    public float speed = 5.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        MoveAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 moveVector = MoveAction.ReadValue<Vector2>();
+        if(moveVector.magnitude != 0){
+            moveVector /= moveVector.magnitude;
+        }
         Debug.Log(moveVector);
-        Vector2 position = (Vector2)transform.position + moveVector * 0.1f;
+        Vector2 position = (Vector2)transform.position + moveVector * speed * Time.deltaTime;
         transform.position = position;
     }
 }
