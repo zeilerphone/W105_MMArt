832f0072652b3fa800895199b7e7798b0b8fcaec
zeilerphone <zeiler@ucsb.edu>
2024-03-02 19:30:47 -0800

    Player Movement Configurable from editor (not working)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
+    public InputAction MoveAction;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +17,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 position = transform.position;
-        position.x += 0.1f;
+        Vector2 moveVector = MoveAction.ReadValue<Vector2>();
+        Debug.Log(moveVector);
+        Vector2 position = (Vector2)transform.position + moveVector * 0.1f;
         transform.position = position;
     }
 }
