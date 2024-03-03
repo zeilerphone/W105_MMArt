fae6f495313761a6c5995fa3d8c2d8c64aa9fe6a
fansenwei17 <fansenwei@ucsb.edu>
2024-03-03 04:13:51 -0500

    I am not sure whether I successfully pushed all my changes.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,7 +11,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        MoveAction.Enable()
     }
 
     // Update is called once per frame
